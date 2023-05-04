6a79e5cf845bdad4a1090d8045f378accc007b4a
Guilherme <guilhermemachado755@gmail.com>
2023-05-04 13:49:24 -0300

    Fix:backend

--- base_clients/src/App.tsx
+++ base_clients/src/App.tsx
@@ -1,20 +1,27 @@
-import { UserProvider } from "./contexts/userContext/userContext";
-import { RoutesMain } from "./routes";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { SearchProvider } from "./contexts/searchContext/searchContext";
-
+import React, { useContext } from 'react';
+import { DetailAnnouncementPage } from './pages/detail_announce';
+import Login from './pages/login_page';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { RegisterPage } from './pages/register_page';
+import { Navbar } from './components/navbar';
+import { AuthContext } from './context/AuthContext';
+import { ProfileViewUserPage } from './pages/profile_view_user_page';
+import { ProtectedRoutes } from './components/protected_routes';
 function App() {
+  const { userAuthenticated } = useContext(AuthContext)
   return (
-    <>
-      <UserProvider>
-        <SearchProvider>
-          <ToastContainer autoClose={2000} />
-          <RoutesMain />
-        </SearchProvider>
-      </UserProvider>
-    </>
-  );
+    <BrowserRouter>
+      <Navbar userAuthenticated={userAuthenticated} />
+      <Routes>
+        <Route path='' element={ userAuthenticated? <Navigate to='/announce-detail' />:<Login />} />
+        <Route path='/register' element={ userAuthenticated? <Navigate to='/announce-detail' />:<RegisterPage />} />
+        <Route element={<ProtectedRoutes />}>
+          <Route path='/announce-detail' element={<DetailAnnouncementPage />} />
+          <Route path='/profile/:id' element={<ProfileViewUserPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- base_clients/src/validations/login.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup'
-
-export const loginSchema = yup.object().shape({
-    email: yup
-        .string()
-        .required("Preenchimento do email obrigatório")
-        .email("Email inválido"),
-    password: yup.string().required("Preenchimento da senha obrigatório"),
-});
